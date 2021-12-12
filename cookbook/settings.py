22f59e5a3c4d8589f285fe361bc4e8d47686cbfc
thiennn-fabbi <thiennn@fabbi.io>
2021-12-12 20:43:12 +0700

    setting vercel

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -43,7 +43,7 @@ DEBUG = env('DEBUG', default=False)
 # SESSION_COOKIE_HTTPONLY = True
 
 CSRF_COOKIE_HTTPONLY = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 CSRF_TRUSTED_ORIGINS = ['localhost']
 CSRF_COOKIE_DOMAIN = 'localhost'
 SESSION_COOKIE_SECURE = True
@@ -100,12 +100,7 @@ WSGI_APPLICATION = 'cookbook.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
